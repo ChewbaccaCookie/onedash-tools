c412ff51dd49142d5f3ea4101f01cc9929570e20
Jonathan Bußhoff <jonathan@busshoff.net>
2020-04-06 22:36:11 +0200

    fix: closing issues fixed dialog and popover

--- src/Utils/Utils.ts
+++ src/Utils/Utils.ts
@@ -1,9 +1,9 @@
 import dayjs, { Dayjs } from "dayjs";
-import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
 
 export default class Utils {
 	static generateGuid = () => {
-		const S4 = function () {
+		const S4 = function() {
 			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 		};
 		return S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4();
@@ -15,7 +15,12 @@ export default class Utils {
 
 	static removeDate = (date: Dayjs | number) => {
 		const d = dayjs(date);
-		return d.set("y", 2000).set("month", 0).set("date", 0).toDate().getTime();
+		return d
+			.set("y", 2000)
+			.set("month", 0)
+			.set("date", 0)
+			.toDate()
+			.getTime();
 	};
 
 	static setTime = (date: Dayjs | number, hours = 0, minutes = 0, seconds = 0, milliseconds = 0) => {
@@ -33,5 +38,8 @@ export default class Utils {
 	};
 	static unlockScrolling = (target?: any) => {
 		enableBodyScroll(target);
+	};
+	static clearAllBodyScrollLocks = () => {
+		clearAllBodyScrollLocks();
 	};
 }
